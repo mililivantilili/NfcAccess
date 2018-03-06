6e33ede791fad814872e939d924408227710409d
mililivantilili <matej.kaloc@gmail.com>
2018-03-06 23:00:54 +0100

    06-03-2018

--- app/src/main/java/com/example/mililivantilili/nfcsecurity/MainActivity.java
+++ app/src/main/java/com/example/mililivantilili/nfcsecurity/MainActivity.java
@@ -1,13 +1,125 @@
 package com.example.mililivantilili.nfcsecurity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
+
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    String TAG = "NfcMain";
+    Tag myTag;
+    NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null)
+        {
+            // NFC is not available on the device.
+            Log.d(TAG,"NFC is not available, Use phone with NFC");
+        }
+        else
+        {
+            pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+            /*readFromIntent(getIntent());
+            IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+            tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+            writeTagFilters = new IntentFilter[] { tagDetected };*/
+            /* Nelze programově zapnout NFC takže tohle otevře nastavení NFC a napíše uživateli, ať ho zapne*/
+            if (!nfcAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), "Please activate NFC and press Back to return to the application!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+            }
+        }
+
+    }
+
+    private void readFromIntent(Intent intent) {
+        Log.d(TAG,"Intent ");
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action))
+        {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null)
+            {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++)
+                {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            Log.d(TAG,"Intent2 ");
+            buildTagViews(msgs);
+        }
+    }
+
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0){
+            Log.d(TAG,"Null nacten ");
+            return;
+        }
+
+        String text = "";
+//        String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+
+        Log.d(TAG,"NFC Content: " + text);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Deaktivace tlačítka zpět
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.d(TAG,"onNewIntent");
+        setIntent(intent);
+        readFromIntent(intent);
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
     }
 }
